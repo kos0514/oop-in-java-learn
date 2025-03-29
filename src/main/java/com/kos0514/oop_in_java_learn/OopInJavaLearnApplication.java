3dc82d6ef3e81dc1c879ec7b680c04d86d0749ad
kos0514 <ghqyydf829@gmail.com>
2025-03-29 16:34:16 +0900

    OOP勉強用プロジェクトの初期コミット

---
+++ src/main/java/com/kos0514/oop_in_java_learn/OopInJavaLearnApplication.java
@@ -0,0 +1,13 @@
+package com.kos0514.oop_in_java_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopInJavaLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopInJavaLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kos0514/oop_in_java_learn/OopInJavaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kos0514.oop_in_java_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopInJavaLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
